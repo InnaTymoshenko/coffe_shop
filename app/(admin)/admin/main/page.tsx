48fd07b3afaa26a112ed0601a145d96fff4e208c
InnaTymoshenko <innatymosh@gmail.com>
2025-05-17 18:49:19 +0300

    fixing forms

--- app/(admin)/admin/main/page.tsx
+++ app/(admin)/admin/main/page.tsx
@@ -5,6 +5,7 @@ import Select from '@/components/ui/select'
 import Shell from '@/components/ui/shell'
 
 const dateRangeOptions = [
+	{ label: 'All time', value: 'all' },
 	{ label: 'Last week', value: 'week' },
 	{ label: 'Last month', value: 'month' },
 	{ label: 'Last year', value: 'year' }
@@ -14,13 +15,29 @@ const MainAdmin = () => {
 	const [dateRange, setDateRange] = useState(dateRangeOptions[0].value)
 
 	return (
-		<Shell className="container">
-			<Select
-				options={dateRangeOptions}
-				value={dateRange}
-				onChange={setDateRange}
-				className="w-1/3 outline-none flex items-center justify-between gap-1 border border-gray-900 rounded-lg px-4 py-2 text-gray-900 bg-gray-50 hover:bg-gray-100"
-			/>
+		<Shell className="container flex flex-col gap-4">
+			<div className="w-full py-4 flex justify-between items-center">
+				<h1 className="text-2xl font-bold">Dashboard</h1>
+			</div>
+			<div className="w-1/3 flex justify-start items-end gap-4">
+				<Select
+					options={dateRangeOptions}
+					value={dateRange}
+					onChange={setDateRange}
+					className="w-1/3 outline-none flex items-center justify-between gap-1 border border-gray-900 rounded-lg px-4 py-2 text-gray-900 bg-gray-50 hover:bg-gray-100"
+				/>
+			</div>
+			<div>
+				<div>
+					<h2>Users</h2>
+				</div>
+				<div>
+					<h2>Orders</h2>
+				</div>
+				<div>
+					<h2>Promotions</h2>
+				</div>
+			</div>
 		</Shell>
 	)
 }
