5f2419c9221128a614ce621b7c8b7e148f83fe00
InnaTymoshenko <innatymosh@gmail.com>
2025-06-06 18:44:43 +0300

    minor fixed

--- app/(cabinet)/cabinet/orders/page.tsx
+++ app/(cabinet)/cabinet/orders/page.tsx
@@ -10,24 +10,24 @@ import { useAdminStore } from '@/store/admin-store'
 const AccountOrdersPage = () => {
 	const [ordersData, setOrdersData] = useState<OrderData[]>([])
 	const [selectedOrder, setSelectedOrder] = useState<OrderData[] | null>(null)
-	const { moskUser } = useAdminStore()
+	const { mockUser } = useAdminStore()
 
 	useEffect(() => {
 		setOrdersData(fakeOrderData as OrderData[])
 	}, [])
 
 	useEffect(() => {
-		if (ordersData && moskUser) {
-			const userOrders = ordersData.filter(order => order.clientId === moskUser.id)
+		if (ordersData && mockUser) {
+			const userOrders = ordersData.filter(order => order.clientId === mockUser.id)
 			setSelectedOrder(userOrders)
 		}
-	}, [moskUser, ordersData])
+	}, [mockUser, ordersData])
 
 	return (
 		<Shell className="container flex flex-col gap-8">
 			<div className="w-full flex flex-col gap-4 justify-start items-start text-gray-200">
 				<h1 className="text-3xl font-bold my-8">My Orders History</h1>
-				{selectedOrder && moskUser && (
+				{selectedOrder && mockUser && (
 					<ul className="w-full ">
 						{selectedOrder.length > 0 &&
 							selectedOrder.map(order => (
